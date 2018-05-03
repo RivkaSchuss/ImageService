6849af2db49310e551232ca1be409c2d4120ce5f
RivkaSchuss <rschuss123@gmail.com>
2018-05-03 12:50:17 +0300

    server

---
+++ ImageService/Tcp/ClientHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Tcp
+{
+    class ClientHandler : IClientHandler
+    {
+        public void HandleClient(TcpClient client)
+        {
+            new Task(() =>
+            {
+                using (NetworkStream stream = client.GetStream())
+                using (StreamReader reader = new StreamReader(stream))
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    //read
+                    //write
+                }
+                client.Close();
+            }).Start();
+        }
+   
+    }
+
+}
