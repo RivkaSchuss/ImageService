5fe74af5ee0eb3dfb849699336e12393c0cdd5ab
RivkaSchuss <rschuss123@gmail.com>
2018-06-04 19:12:09 +0300

    weird version

--- ImageServiceWeb/Models/PhotosModel.cs
+++ ImageServiceWeb/Models/PhotosModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,15 @@ namespace ImageServiceWeb.Models
 {
     public class PhotosModel
     {
-        
-        //public event NotifyAboutChange NotifyEvent;
 
+        //public event NotifyAboutChange NotifyEvent;
+        private string outputDir;
+        private static ConfigModel config;
         public PhotosModel()
         {
             ImageList = GetPhotos();
+            config = new ConfigModel();
+            outputDir = config.OutputDirectory;
         }
 
         public List<Image> ImageList
@@ -26,7 +30,34 @@ namespace ImageServiceWeb.Models
         public List<Image> GetPhotos()
         {
             List<Image> photosList = new List<Image>();
+            string thumbnailDir = outputDir + "\\Thumbnails";
+            if (!Directory.Exists(thumbnailDir))
+            {
+                return null;
+            }
+            DirectoryInfo di = new DirectoryInfo(thumbnailDir);
 
+            string[] validExtensions = { ".jpg", ".png", ".gif", ".bmp" };
+            foreach (DirectoryInfo yearDirInfo in di.GetDirectories())
+            {
+                if (!Path.GetDirectoryName(yearDirInfo.FullName).EndsWith("Thumbnails"))
+                {
+                    continue;
+                }
+                foreach (DirectoryInfo monthDirInfo in yearDirInfo.GetDirectories())
+                {
+
+
+                    foreach (FileInfo fileInfo in monthDirInfo.GetFiles())
+                    {
+                        if (validExtensions.Contains(fileInfo.Extension.ToLower()))
+                        {
+                            
+                            //photosList.Add(new Image(fileInfo.FullName));
+                        }
+                    }
+                }
+            }
 
 
             return photosList;
