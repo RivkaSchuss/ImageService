456ca2668a9a9b5563a0446b0a45826a9216b62d
RivkaSchuss <rschuss123@gmail.com>
2018-06-04 15:41:39 +0300

    config

--- ImageServiceWeb/Models/ConfigModel.cs
+++ ImageServiceWeb/Models/ConfigModel.cs
@@ -1,15 +1,29 @@
 ﻿using Communication.Client;
+using Infrastructure;
+using Infrastructure.Enums;
+using Infrastructure.Event;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Windows;
+using System.Windows.Threading;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ImageServiceWeb.Models
 {
     public class ConfigModel
     {
-        public delegate void NotifyAboutChange();
         private IImageServiceClient client;
         private List<string> handlers;
 
@@ -17,6 +31,9 @@ namespace ImageServiceWeb.Models
         {
             client = ImageServiceClient.Instance;
             handlers = new List<string>();
+            this.client.DataReceived += NotifyChange;
+            CommandReceivedEventArgs request = new CommandReceivedEventArgs((int)CommandEnum.GetConfigCommand, null, null);
+            this.client.Initialize(request);
         }
 
         public List<string> Handlers
@@ -26,6 +43,37 @@ namespace ImageServiceWeb.Models
                 return this.handlers;
             }
         }
+
+        public void NotifyChange(object sender, CommandMessage message)
+        {
+            if (message.CommandID.Equals((int)CommandEnum.GetConfigCommand))
+            {
+                try
+                {
+                    Application.Current.Dispatcher.Invoke(new Action(() =>
+                    {
+                        //Console.WriteLine("I am here I am hereI am hereI am hereI am hereI am hereI am hereI am hereI am hereI am hereI am here");
+                        this.OutputDirectory = (string)message.CommandArgs["OutputDirectory"];
+                        this.SourceName = (string)message.CommandArgs["SourceName"];
+                        this.LogName = (string)message.CommandArgs["LogName"];
+                        this.ThumbnailSize = (int)message.CommandArgs["ThumbnailSize"];
+                        JArray arr = (JArray)message.CommandArgs["Handlers"];
+                        string[] array = arr.Select(c => (string)c).ToArray();
+                        foreach (var item in array)
+                        {
+                            this.Handlers.Add(item);
+                        }
+
+                    }));
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
 
 
         [Required]
